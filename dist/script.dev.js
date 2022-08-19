8b6bf31260ab9425a5532e9cdcf4b926d3bdc5a3
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-08-19 12:51:45 +0100

    used strings instead of arrays and added switch case

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -7,72 +7,57 @@ var clearButton = document.querySelector("#calculator-AC");
 var equals = document.querySelector(".calculator-equals");
 console.log(operators); //let firstNumber forEach  add eventListener which stores value when click on butt0n
 
-var totalArray = [];
-var firstNumber = [];
-var secondNumber = [];
-numbers.forEach(function (number) {
-  number.addEventListener("click", function () {
-    if (!totalArray.includes(operators)) {
-      firstNumber.push(number.value);
-      totalArray.push(number.value);
-      output.textContent = totalArray.join("");
-      return firstNumber;
-    } else {
-      secondNumber.push(number.value);
-      totalArray.push(number.value);
-      output.textContent = totalArray.join("");
-      return secondNumber;
-    }
-  });
-});
-console.log(totalArray);
-console.log(firstNumber);
-console.log(secondNumber);
-var operatorArr = [];
-operators.forEach(function (operator) {
-  operator.addEventListener("click", function () {
-    operatorArr.push(operator.value);
-    totalArray.push(operator.value);
-    output.textContent = totalArray.join("");
-    return operatorArr;
-  });
-});
-console.log(operatorArr); //this will clear the display and clear the arrays to start over each time!
+var totalOutput = "";
+var firstNumber = "";
+var secondNumber = "";
+var operator = ""; //NUMBERS
 
-clearButton.addEventListener("click", function (event) {
-  output.textContent = " ";
-  firstNumber = [];
-  secondNumber = [];
-  operatorArr = [];
-  totalArray = [];
-}); //switch case function= pull when equals is clicked
+var handleNumberPress = function handleNumberPress(event) {
+  var inputtedNumber = event.target.value;
 
-var handleOperator = function handleOperator() {
-  switch (operators) {
-    case totalArray.includes("+"):
-      functionToPull = addition;
-      break;
-
-    case totalArray.includes("-"):
-      functionToPull = subtraction;
-      break;
-
-    case totalArray.includes("÷"):
-      functionToPull = division;
-      break;
-
-    case totalArray.includes("x"):
-      functionToPull = multiplication;
-      break;
+  if (operator === "") {
+    firstNumber += inputtedNumber.toString();
+    totalOutput = firstNumber;
+    output.textContent = totalOutput;
+    console.log("is getting here", firstNumber);
+  } else {
+    secondNumber += inputtedNumber.toString();
+    totalOutput = firstNumber + operator + secondNumber;
+    output.textContent = totalOutput;
+    console.log("is here instead", secondNumber);
   }
 };
 
-equals.addEventListener("click", handleOperator);
+numbers.forEach(function (number) {
+  number.addEventListener("click", handleNumberPress);
+});
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(operator); //OPERATORS 
+
+var handleOperatorPress = function handleOperatorPress(event) {
+  console.log(event);
+  console.log(event.target.value);
+  operator = event.target.value;
+  totalOutput = firstNumber + operator;
+  output.textContent = totalOutput;
+};
+
+operators.forEach(function (operator) {
+  operator.addEventListener("click", handleOperatorPress);
+}); //CLEAR THE DISPLAY!
+
+clearButton.addEventListener("click", function (event) {
+  output.textContent = " ";
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  totalOutput = "";
+}); //switch case function= pull when equals is clicked
 
 var addition = function addition(firstNumber, secondNumber) {
   var additionAnswer = firstNumber + secondNumber;
   output.textContent = additionAnswer;
-  return additionAnswer;
 };
 
 console.log(additionAnswer);
@@ -85,12 +70,36 @@ var subtraction = function subtraction(firstNumber, secondNumber) {
 
 console.log(subtractionAnswer);
 
-var multiplication = function multiplication(number1, number2) {
-  var multiplicationAnswer = number1 * number2;
+var multiplication = function multiplication(firstNumber, secondNumber) {
+  var multiplicationAnswer = firstNumber * secondNumber;
   return multiplicationAnswer;
 };
 
-var division = function division(number1, number2) {
-  var divisionAnswer = number1 / number2;
+console.log(multiplicationAnswer);
+
+var division = function division(firstNumber, secondNumber) {
+  var divisionAnswer = firstNumber / secondNumber;
   return divisionAnswer;
 };
+
+var handleOperator = function handleOperator() {
+  switch (operator) {
+    case "+":
+      functionToPull = addition;
+      break;
+
+    case "-":
+      functionToPull = subtraction;
+      break;
+
+    case "÷":
+      functionToPull = division;
+      break;
+
+    case "x":
+      functionToPull = multiplication;
+      break;
+  }
+};
+
+equals.addEventListener("click", handleOperator);
--- script.js
+++ script.js
@@ -7,48 +7,59 @@ console.log(operators);
 
 //let firstNumber forEach  add eventListener which stores value when click on butt0n
 
-let totalArray = [];
+let totalOutput = "";
 
-let firstNumber = [];
-let secondNumber = [];
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+//NUMBERS
+const handleNumberPress = (event) => {
+    let inputtedNumber = event.target.value;
+   if (operator === "") {
+    firstNumber += inputtedNumber.toString(); 
+    totalOutput = firstNumber;
+    output.textContent = totalOutput;          
+    console.log("is getting here", firstNumber);
+   } else {
+    secondNumber += inputtedNumber.toString();
+    totalOutput = firstNumber + operator + secondNumber;
+    output.textContent = totalOutput;
+    console.log("is here instead", secondNumber);
+   }
+}
 
 numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-    if (!totalArray.includes(operators)) {
-      firstNumber.push(number.value);
-      totalArray.push(number.value);
-      output.textContent = totalArray.join("");
-      return firstNumber;
-    } else {
-      secondNumber.push(number.value);
-      totalArray.push(number.value);
-      output.textContent = totalArray.join("");
-      return secondNumber;
-    }
+    number.addEventListener("click", handleNumberPress);
   });
-});
-console.log(totalArray);
-console.log(firstNumber);
-console.log(secondNumber);
 
-let operatorArr = [];
+  console.log(firstNumber);
+  console.log(secondNumber);
+  console.log(operator);
+
+//OPERATORS 
+
+const handleOperatorPress = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+    operator = event.target.value;
+    totalOutput = firstNumber + operator;
+    output.textContent = totalOutput;
+}
+
 operators.forEach((operator) => {
-  operator.addEventListener("click", () => {
-    operatorArr.push(operator.value);
-    totalArray.push(operator.value);
-    output.textContent = totalArray.join("");
-    return operatorArr;
-  });
+  operator.addEventListener("click", handleOperatorPress)
 });
-console.log(operatorArr);
 
-//this will clear the display and clear the arrays to start over each time!
+
+
+//CLEAR THE DISPLAY!
 clearButton.addEventListener("click", (event) => {
   output.textContent = " ";
-  firstNumber = [];
-  secondNumber = [];
-  operatorArr = [];
-  totalArray = [];
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  totalOutput = "";
 });
 
 
@@ -56,30 +67,10 @@ clearButton.addEventListener("click", (event) => {
 
 //switch case function= pull when equals is clicked
 
-const handleOperator = () => {
-
-switch (operators) {
-    case totalArray.includes("+"):
-        functionToPull = addition;
-        break;
-    case totalArray.includes("-"):
-        functionToPull = subtraction;
-        break;
-    case totalArray.includes("÷"):
-        functionToPull = division;
-        break;
-    case totalArray.includes("x"):
-        functionToPull = multiplication;
-        break;
-}
-}
-
-equals.addEventListener("click", handleOperator);
 
 const addition = (firstNumber, secondNumber) => {
   let additionAnswer = firstNumber + secondNumber;
   output.textContent = additionAnswer;
-  return additionAnswer;
 };
 console.log(additionAnswer);
 
@@ -91,14 +82,35 @@ const subtraction = (firstNumber, secondNumber) => {
 console.log(subtractionAnswer);
 
 
-const multiplication = (number1, number2) => {
-  let multiplicationAnswer = number1 * number2;
+const multiplication = (firstNumber, secondNumber) => {
+  let multiplicationAnswer = firstNumber * secondNumber;
   return multiplicationAnswer;
 };
+console.log(multiplicationAnswer);
 
 
-const division = (number1, number2) => {
-  let divisionAnswer = number1 / number2;
+const division = (firstNumber, secondNumber) => {
+  let divisionAnswer = firstNumber / secondNumber;
   return divisionAnswer;
 };
+
+const handleOperator = () => {
+
+    switch (operator) {
+        case "+":
+            functionToPull = addition;
+            break;
+        case "-":
+            functionToPull = subtraction;
+            break;
+        case "÷":
+            functionToPull = division;
+            break;
+        case "x":
+            functionToPull = multiplication;
+            break;
+    }
+    }
+   
+    equals.addEventListener("click", handleOperator);
 
